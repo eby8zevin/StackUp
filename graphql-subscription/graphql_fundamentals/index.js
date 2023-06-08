c78c750601369be2ec59b69e689efb1e56c4e5bd
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-06-08 08:12:40 +0700

    Build a GraphQL Subscription Server

--- graphql-subscription/graphql_fundamentals/index.js
+++ graphql-subscription/graphql_fundamentals/index.js
@@ -1,49 +1,100 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { json } = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+const { PubSub } = require("graphql-subscriptions");
+const pubsub = new PubSub();
 
-// Create movies array
 const movies = [
   {
-    movieTitle: "Avengers - EndGame",
-    dateOfRelease: "Dec 22",
+    movieTitle: "Avengers-Endgame",
+    dateOfRelease: "2019",
   },
 ];
 
-// Initialize type definitions
-const typeDefs = gql`
-  type Movie {
-    movieTitle: String!
-    dateOfRelease: String!
-  }
+const main = async () => {
+  const typeDefs = `#graphql
+    type Movie {
+      movieTitle: String!
+      dateOfRelease: String!
+    }
 
-  type Query {
-    queryMovies: [Movie]!
-  }
+    type Query {
+      queryMovies: [Movie]!
+    }
 
-  type Mutation {
-    addMovie(movieTitle: String!, dateOfRelease: String!): Movie!
-  }
-`;
+    type Mutation {
+      addMovie(movieTitle: String!, dateOfRelease: String!): Movie!
+    }
+    
+    type Subscription {
+      newMovie: [Movie!]!
+    }   
+  `;
 
-// Create resolver functions
-const resolvers = {
-  Query: {
-    queryMovies: () => movies,
-  },
-
-  Mutation: {
-    addMovie: (parent, args) => {
-      movies.push(args);
-      return args;
+  const resolvers = {
+    Query: {
+      queryMovies: () => movies,
     },
-  },
+
+    Mutation: {
+      addMovie: (parent, args) => {
+        movies.push(args);
+        pubsub.publish("MOVIE_ADDED", { newMovie: movies });
+        return args;
+      },
+    },
+
+    Subscription: {
+      newMovie: {
+        subscribe: () => pubsub.asyncIterator(["MOVIE_ADDED"]),
+      },
+    },
+  };
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  const httpServer = createServer(app);
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/",
+  });
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  await server.start();
+
+  app.use("/", cors(), json(), expressMiddleware(server));
+
+  const PORT = process.env.PORT || 8080;
+  httpServer.listen(PORT, () => {
+    console.log(`GraphQL server running at ${PORT}`);
+  });
 };
 
-// Initialize GraphQL server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server
-  .listen({ port: 8080 })
-  .then(({ url }) => console.log(`GraphQL server running at ${url}`));
+main();
