3314260d283a0b2237218106d1badd0df4681265
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-06-13 20:51:49 +0700

    Import Statements

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/Header";
+import Draggable from "react-draggable";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
