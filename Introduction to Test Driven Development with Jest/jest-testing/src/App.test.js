073fee361a4212230f00d7124cd827d7c501c1c0
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-07-19 22:10:12 +0700

    Testing Multiple React Components

--- Introduction to Test Driven Development with Jest/jest-testing/src/App.test.js
+++ Introduction to Test Driven Development with Jest/jest-testing/src/App.test.js
@@ -2,18 +2,30 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
-
-jest.mock("axios", () => ({
-  get: () => Promise.resolve({ data: "Mocked Data" }),
-}));
+import App, { Title } from "./App";
 
 describe("App component", () => {
-  it("renders API data", async () => {
-    const { findByText } = render(<App />);
+  it("renders Logged out", () => {
+    render(<App />);
 
-    const dataElement = await findByText("Mocked Data");
+    expect(screen.getByRole("heading").textContent).toMatch("Logged Out");
+  });
 
-    expect(dataElement).toBeTruthy();
+  it("renders logged in after button click", async () => {
+    const user = userEvent;
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Log In" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch("Logged In");
+  });
+});
+
+describe("Title Component", () => {
+  it("renders Hello", () => {
+    render(<Title />);
+    expect(screen.getByRole("heading").textContent).toMatch("Hello");
   });
 });
