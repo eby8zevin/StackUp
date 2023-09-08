019815619d27d36c646c9261745f7b7a1d9f7f42
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-08 21:35:43 +0700

    Classes and Constructors

---
+++ JavaScript Intermediate/JavaScript Classes/index.js
@@ -0,0 +1,14 @@
+class MyClass {
+  constructor(property1, property2) {
+    this.property1 = property1;
+    this.property2 = property2;
+  }
+
+  method1() {}
+
+  method2() {}
+}
+
+const myInstance = new MyClass("value1", "value2");
+console.log(myInstance.property1);
+myInstance.method1();
