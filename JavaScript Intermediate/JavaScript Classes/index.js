edf71ae1d03f5a9d6c0abb70897573101e3b5899
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-08 21:37:39 +0700

    Our First Class in JavaScript

--- JavaScript Intermediate/JavaScript Classes/index.js
+++ JavaScript Intermediate/JavaScript Classes/index.js
@@ -12,3 +12,12 @@ class MyClass {
 const myInstance = new MyClass("value1", "value2");
 console.log(myInstance.property1);
 myInstance.method1();
+
+class Book {
+  constructor(title, author, ISBN, availability = true) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.availability = availability;
+  }
+}
