e5b4f76e99902e8904e0eabda1de408f1f45c89b
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-14 19:39:25 +0700

    Application of Try-Catch Statement

--- JavaScript Intermediate/Try-Catch-Finally Statements/index.js
+++ JavaScript Intermediate/Try-Catch-Finally Statements/index.js
@@ -2,4 +2,24 @@ document.getElementById("button").addEventListener("click", function () {
   let input = document.getElementById("input").value;
   let output = document.getElementById("output");
   let sequence = [];
+
+  try {
+    if (input < 0) {
+      throw new Error("Input cannot be negative");
+    } else if (input === "") {
+      throw new Error("Input cannot be empty");
+    } else if (isNaN(input)) {
+      throw new Error("Input must be a number");
+    } else {
+    }
+    output.innerHTML = sequence.join(", ");
+  } catch (error) {
+    output.innerHTML = error;
+  } finally {
+    alert(
+      `The ${input}th number in the fibonacci sequence is ${
+        sequence[input - 1]
+      }`
+    );
+  }
 });
